c318642a3f6b119c7415cecbe40a7f6e12718495
jonardlolz <craigejonard@gmail.com>
2022-04-01 23:58:17 +0800

    added delete func on purpose module

--- database/ebarangaydb (11).sql
+++ database/ebarangaydb (11).sql
@@ -207,7 +207,7 @@ CREATE TABLE `notifications` (
   `position` varchar(50) DEFAULT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `notifications`
@@ -352,7 +352,7 @@ CREATE TABLE `report` (
   `updated_on` datetime DEFAULT CURRENT_TIMESTAMP,
   `userBarangay` varchar(50) DEFAULT NULL,
   `userPurok` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `report`
@@ -452,7 +452,7 @@ CREATE TABLE `schedule` (
   `complainee` int DEFAULT NULL,
   `forAll` varchar(10) DEFAULT 'False',
   `scheduleTitle` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `schedule`
