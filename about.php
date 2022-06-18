2a2426cfe89a227fcc8bbaf3f19215508e7bfccb
Jonard <craigejonard@gmail.com>
2022-06-18 12:15:13 +0800

    06-18-22

---
+++ about.php
@@ -0,0 +1,73 @@
+<?php include 'header.php' ?>
+
+<style>
+    .col img{
+        width: 150px;
+        height: 150px;
+    }
+</style>
+<div class="col d-flex flex-column px-4">
+    <div class="card shadow mb-4 m-4 p-4">
+        <span>About eBarangay</span>
+        <div class="d-flex justify-content-center m-3 text-center">
+            <img class="img-profile" src="img/eb-logo.png" alt="" srcset="">
+        </div>
+        <div class="d-flex justify-content-center m-3 text-center">
+            <small>eBarangay: An Application Management System <br> with Purok Integration and Flow of Workload</small>
+        </div>
+        <div class="d-flex border border-primary m-3 p-4" style="text-align: justify;">
+            <p>We are a group of young developers that are aspiring to create web-based barangay management system for our Capstone 2 subject. In our pursuit of creating this program, we have faced challenges, difficulties, and our lack of experience. Yet, despite all of this, we are happy with what we have created and shared to this world, as well as satisfied from the experience and knowledge we have attained from this project. We hope to create more kinds of projects and improve upon it in the future.
+                <br><br>
+            Barangays are an essential to a resident. They provide a link between the people and government, address the its community’s problems and concerns, enforce policies and hold influence over its communities, as well as provide government documents to submit. As with most services that are publicly available today, most of it has gone digital and most people found the convenience useful compared to the traditional way. This convenience has become a lot more valuable with what we’ve experienced during the pandemic, as the residents are mostly forced to stay within their homes for their safety. This study aims to develop a convenient web-based Barangay Management System platform. The Barangay Officials can manage their profile, residents, documents, complaints, election, barangay, and purok. Residents can manage their own profile, document requests, complaints, and voting. Admin can manage Barangay Captains, Barangay, and Purok. In developing the application, the researchers used the following tools: HTML, CSS, Bootstrap, JQuery, and Javascript. While the following tools are used for backend: PHP and MySQL. The researchers strongly recommend to implement or deploy the system and recommend to use the eBarangay for the Barangay Hall to easily monitor and manage profiling for their own barangay residents. For the future researchers, we recommend to provide online payments options such as PayPal, Pay Maya, and etc.  
+            </p>
+        </div>
+        <div class="d-flex justify-content-evenly">
+            <div class="card">
+                <div class="card-header text-center">
+                    <img class="rounded m-2" src="./img/1633943100_gru.png" alt="">
+                </div>
+                <div class="card-body text-center">
+                    <div class="row">
+                        <b>Craige Jonard N. Baring</b>
+                    </div>
+                    <div class="row">
+                        <small>Hacker</small>
+                    </div>
+                    <div class="row">
+                        <small>BSIT Student</small>
+                    </div>
+                    <div class="row">
+                        <small>
+                            Cebu Technological University
+                            Main Campus
+                        </small>
+                    </div>
+                </div>
+            </div>
+           <div class="card">
+                <div class="card-header text-center">
+                    <img class="rounded m-2" src="./img/1633945260_Untitled.png" alt="">
+                </div>
+                <div class="card-body text-center">
+                    <div class="row">
+                        <b>Rosvie R. Pepito</b>
+                    </div>
+                    <div class="row">
+                        <small>Hipster</small>
+                    </div>
+                    <div class="row">
+                        <small>BSIT Student</small>
+                    </div>
+                    <div class="row">
+                        <small>
+                            Cebu Technological University
+                            Main Campus
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php' ?>
